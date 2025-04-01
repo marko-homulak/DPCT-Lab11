1959306d1f5ccc051db0bacfb441f84911529bd9
Mark <73536533+marko-homulak@users.noreply.github.com>
2025-04-01 19:34:32 +0300

    Add files via upload

---
+++ Print.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lab11
+{
+    public static class Print
+    {
+        public static void Vector(double[] vector, string label = "Vector[]")
+        {
+            Console.Write($"{label}:" + " { ");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (i + 1 == vector.Length)
+                    Console.Write($"{vector[i]}" + " }");
+                else
+                    Console.Write($"{vector[i]}; ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void Matrix(double[,] matrix, string label = "Matrix[,]")
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            Console.Write($"{label}:\n");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    string formattedValue = matrix[i, j].ToString("F6");
+                    int padding = matrix[i, j] < 0 ? 3 : 4;
+                    Console.Write($"{formattedValue.PadLeft(10 + padding)}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using lab11;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    const int n = 6;
+    const int m = 6;
+    static double[,] A = new double[n, m];
+    static double[] B = new double[m];
+    static double[] resultRow = new double[n];
+    static double[] resultBlock = new double[n];
+
+    static void Main()
+    {
+        double x = 5;
+        GenerateMatrixAndVector(x);
+
+        MultiplyMatrixVectorRowWise();
+        Console.WriteLine("\nMultiplication result (row division):");
+        Print.Vector(resultRow, "Vector[C1]");
+
+        MultiplyMatrixVectorBlockWise();
+        Console.WriteLine("\nMultiplication result (block division):");
+        Print.Vector(resultBlock, "Vector[C2]");
+
+        Console.ReadKey();
+    }
+
+    static void GenerateMatrixAndVector(double x)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                A[i, j] = Math.Round((Math.Pow(i, 2) - Math.Pow(x, 3) + 1) / (j + Math.Sin(Math.Pow(x, 0.2))) - Math.Pow(x, 1.3), 4);
+            }
+        }
+
+        Print.Matrix(A, "Matrix[A]");
+
+        for (int j = 0; j < m; j++)
+        {
+            double denominator = j + Math.Pow(Math.Sin(4 * j), 0.2);
+
+            if (denominator == 0 || double.IsNaN(denominator))
+            {
+                //Console.WriteLine($"Warning: Division by zero or NaN at j = {j}");
+                denominator = 1;
+                continue;
+            }
+
+            B[j] = Math.Round((Math.Pow(x, 2) - Math.Pow(2, j) + 1) / denominator, 4);
+        }
+
+        Print.Vector(B, "Vector[B]");
+    }
+
+    static void MultiplyMatrixVectorRowWise()
+    {
+        Parallel.For(0, n, i =>
+        {
+            double sum = 0;
+            for (int j = 0; j < m; j++)
+            {
+                sum += A[i, j] * B[j];
+            }
+            resultRow[i] = sum;
+        });
+    }
+
+    static void MultiplyMatrixVectorBlockWise()
+    {
+        int numThreads = Environment.ProcessorCount;
+        int blockSize = n / numThreads;
+        Parallel.For(0, numThreads, t =>
+        {
+            int start = t * blockSize;
+            int end = (t == numThreads - 1) ? n : start + blockSize;
+
+            for (int i = start; i < end; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    sum += A[i, j] * B[j];
+                }
+                resultBlock[i] = sum;
+            }
+        });
+    }
+}
